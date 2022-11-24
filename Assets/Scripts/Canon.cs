5f4d50144251401fedde1b30892930b5c619ff3b
HeyLiba <118818963+HeyLiba@users.noreply.github.com>
2022-11-24 14:12:25 +0000

    add canon

---
+++ Assets/Scripts/Canon.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+    [SerializeField] Transform shootPoint;
+    [SerializeField] Rigidbody2D bulletPrefab;
+    [SerializeField] float shootForce = 10f;
+    [SerializeField] float rayDistance = 10f;
+    [SerializeField] private float shootCoolDown = 2f;
+    private float lastShoot = -999f;
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(shootPoint.position, Vector2.left, rayDistance);
+        if (hit.collider)
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                float now = Time.time;
+                if (lastShoot + shootCoolDown < now)
+                {
+                    Shoot();
+                    lastShoot = now;
+                }
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Rigidbody2D bullet = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+        bullet.AddForce(shootForce * shootPoint.transform.up, ForceMode2D.Impulse);
+    }
+}
