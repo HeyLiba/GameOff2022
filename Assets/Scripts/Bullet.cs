ea6bc7d9d7b6716788f8fa32a170bbcea50949b5
HeyLiba <118818963+HeyLiba@users.noreply.github.com>
2022-11-25 12:21:17 +0000

    add Temperature

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float level = 1f;
     [SerializeField] Color expiredColor = Color.gray;
+    private bool expired = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        gameObject.layer = LayerMask.NameToLayer("ExpiredBullet");
-        GetComponent<SpriteRenderer>().color = expiredColor;
-
         //NOTE: only ExpiredBullet layer can collide with player and enemy! 
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
         {
+            if (!expired)
+            {
+                collision.gameObject.GetComponent<Temperature>().Heat(level, collision.GetContact(0).point);
+            }
             Destroy(gameObject);
-            // add bullet to inventory
+        }
+        else
+        {
+            gameObject.layer = LayerMask.NameToLayer("ExpiredBullet");
+            GetComponent<SpriteRenderer>().color = expiredColor;
+            expired = true;
         }
     }
 }
---
+++ Assets/Scripts/Temperature.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temperature : MonoBehaviour
+{
+    public float MaxTemperature = 3f;
+    private float temperature = 0f;
+    public virtual void Heat(float level, Vector2 position)
+    {
+        temperature += level;
+        CheckTemperature();
+    }
+
+    private void CheckTemperature()
+    {
+        if (temperature > MaxTemperature)
+        {
+            OverHeat();
+        }
+    }
+
+    public virtual void OverHeat()
+    {
+        Debug.Log(name + " Destroyed :(");
+        Destroy(gameObject);
+    }
+}
